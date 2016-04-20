79d4ed46e5d779cec90d0a980c5e1c02d0b1a429
Spencer Caldwell <spencer.caldwell@gmail.com>
2016-04-20 12:43:18 -0700

    added axes

---
+++ 04_scales_and_axes/adding_axes.js
@@ -0,0 +1,84 @@
+var h = 100;
+var w = 400;
+var padding = 20;
+
+//generate month names from numbers
+function getDate(d) {
+  var stringDate = new String(d);
+  var year = stringDate.substr(0, 4);
+  var month = stringDate.substr(4, 2);
+  var day = stringDate.substr(6, 2);
+
+  return new Date(year, month, day);
+}
+
+//show header information
+function showHeader(ds) {
+  d3.select('body').append('h1').text(ds.category + ' Sales 2013');
+}
+
+//builds the line
+function buildLine(ds) {
+
+  var minDate = getDate(ds.monthlySales[0]['month']);
+  var maxDate = getDate(ds.monthlySales[ds.monthlySales.length - 1]['month']);
+
+  //scaling functions
+  var xScale = d3.time.scale()
+    .domain([minDate, maxDate])
+    .range([padding, w-padding]);
+
+  var yScale = d3.scale.linear()
+    .domain([
+      0,
+      d3.max(ds.monthlySales, function(d) {return d.sales;})
+    ])
+    .range([h-padding, 10]);
+
+  //axes functions construction
+  var xAxisGen = d3.svg.axis().scale(xScale).orient('bottom').tickFormat(d3.time.format('%b'));
+
+  var yAxisGen = d3.svg.axis().scale(yScale).orient('left').ticks(4);
+
+  //creates the svg on the page
+  var svg = d3.select('body').append('svg').attr({height: h, width:w});
+
+  //constructs the line from data
+  var lineFunction = d3.svg.line()
+    .x(function(d) {return xScale(getDate(d.month));})
+    .y(function(d) {return yScale(d.sales);})
+    .interpolate('linear');
+
+  //draw the axes
+  var xAxis = svg.append('g').call(xAxisGen)
+    .attr("class", "axis")
+    .attr("transform", "translate(0, " +(h-padding)+ ")");
+
+  var yAxis = svg.append('g').call(yAxisGen)
+    .attr("class", "axis")
+    .attr("transform", "translate(" + padding + ", 0)");
+
+  //draw the line
+  var viz = svg.append('path')
+    .attr({
+      //pass in data
+      d: lineFunction(ds.monthlySales),
+      'stroke': 'purple',
+      'stroke-width': 2,
+      'fill': 'none'
+    });
+}
+
+//retrieves data and calls functions
+d3.json("https://api.github.com/repos/bsullins/d3js-resources/contents/monthlySalesbyCategoryMultiple.json", function(err, data) {
+  if (err) {console.log(err);}
+  if (data) {console.log(data);}
+
+  var decodedData = JSON.parse(window.atob(data.content));
+
+  for (var i = 0; i < decodedData.contents.length; i++) {
+    showHeader(decodedData.contents[i]);
+    buildLine(decodedData.contents[i]);
+  }
+
+});
