e34c9d570643faf03c6cf37d95fd1541ae49f2e5
Spencer Caldwell <spencer.caldwell@gmail.com>
2016-04-26 22:48:03 -0700

    added hover over tooltip

---
+++ 05_interactivity/tooltips.js
@@ -0,0 +1,27 @@
+var w = 300;
+var h = 100;
+var padding = 2;
+var dataset = [5, 10, 15, 20, 25, 7, 32, 25];
+svg = d3.select('body').append('svg').attr('width', w).attr('height', h);
+
+function colorPicker(v) {
+  if(v<=20) {return '#666666';}
+  if(v>20) {return '#FF0033';}
+}
+
+svg.selectAll('rect')
+    .data(dataset)
+    .enter()
+    .append('rect')
+    .attr({
+      x: function(d, i) {return (i * (w/dataset.length));},
+      y: function(d) {return (h-d);},
+      width: (w / dataset.length - padding),
+      height: function(d) {return d;},
+      fill: function(d) {return colorPicker(d);}
+    })
+    //adds hover over tooltip
+    .append('title')
+    .text(function(d) {
+      return d;
+    });
