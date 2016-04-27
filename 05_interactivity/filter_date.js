0fb01c5c5baa6466c1a801c1997432d30c8b55e4
Spencer Caldwell <spencer.caldwell@gmail.com>
2016-04-26 22:26:14 -0700

    added a time period selector

--- 05_interactivity/filter_date.js
+++ 05_interactivity/filter_date.js
@@ -41,7 +41,7 @@ function buildLine(ds) {
   var yAxisGen = d3.svg.axis().scale(yScale).orient('left').ticks(4);
 
   //creates the svg on the page
-  var svg = d3.select('body').append('svg').attr({height: h, width:w});
+  var svg = d3.select('body').append('svg').attr({height: h, width:w, "id":"svg-" + ds.category});
 
   //constructs the line from data
   var lineFunction = d3.svg.line()
@@ -51,11 +51,11 @@ function buildLine(ds) {
 
   //draw the axes
   var xAxis = svg.append('g').call(xAxisGen)
-    .attr("class", "axis")
+    .attr("class", "x-axis")
     .attr("transform", "translate(0, " +(h-padding)+ ")");
 
   var yAxis = svg.append('g').call(yAxisGen)
-    .attr("class", "axis")
+    .attr("class", "y-axis")
     .attr("transform", "translate(" + padding + ", 0)");
 
   //draw the line
@@ -65,7 +65,53 @@ function buildLine(ds) {
       d: lineFunction(ds.monthlySales),
       'stroke': 'purple',
       'stroke-width': 2,
-      'fill': 'none'
+      'fill': 'none',
+      'class': 'path-' + ds.category
+    });
+}
+
+//updates the line
+function updateLine(ds) {
+
+  var minDate = getDate(ds.monthlySales[0]['month']);
+  var maxDate = getDate(ds.monthlySales[ds.monthlySales.length - 1]['month']);
+
+  //scaling functions
+  var xScale = d3.time.scale()
+    .domain([minDate, maxDate])
+    .range([padding, w-padding]);
+
+  var yScale = d3.scale.linear()
+    .domain([
+      0,
+      d3.max(ds.monthlySales, function(d) {return d.sales;})
+    ])
+    .range([h-padding, 10]);
+
+  //axes functions construction
+  var xAxisGen = d3.svg.axis().scale(xScale).orient('bottom').tickFormat(d3.time.format('%b')).ticks(ds.monthlySales.length - 1);
+
+  var yAxisGen = d3.svg.axis().scale(yScale).orient('left').ticks(4);
+
+  //creates the svg on the page
+  var svg = d3.select('body').select('#svg-' + ds.category);
+
+  //constructs the line from data
+  var lineFunction = d3.svg.line()
+    .x(function(d) {return xScale(getDate(d.month));})
+    .y(function(d) {return yScale(d.sales);})
+    .interpolate('linear');
+
+  //draw the axes
+  var xAxis = svg.selectAll('g.x-axis').call(xAxisGen);
+
+  var yAxis = svg.selectAll('g.y-axis').call(yAxisGen);
+
+  //draw the line
+  var viz = svg.selectAll('.path-' + ds.category)
+    .attr({
+      //pass in data
+      d: lineFunction(ds.monthlySales)
     });
 }
 
@@ -81,4 +127,23 @@ d3.json("https://api.github.com/repos/bsullins/d3js-resources/contents/monthlySa
     buildLine(decodedData.contents[i]);
   }
 
+  d3.select('select')
+    .on('change', function(d, i) {
+      var sel = d3.select('#date-option').node().value;
+
+      var decodedData = JSON.parse(window.atob(data.content));
+
+      // console.log(decodedData);
+      // for (var i = 0; i < decodedData.contents.length; i++) {
+      //   decodedData.monthlySales.splice(0, decodedData.monthlySales.lenght-sel);
+      //   updateLine(decodedData.contents[i]);
+      // }
+
+      //working on converting this into a native for loop above
+      decodedData.contents.forEach(function(ds) {
+        ds.monthlySales.splice(0,ds.monthlySales.length-sel);
+        updateLine(ds);
+      });
+
+    });
 });
